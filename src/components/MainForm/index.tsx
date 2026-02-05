71e6fcdc5b0b4a61cde49d2582a14eaed79288d3
Heitor Macedo <heitorvmacedo@gmail.com>
2026-02-05 15:55:30 -0300

    52. Input controlado no React com 'value', 'onChange' e 'useState'

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,16 +2,28 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useState } from "react";
 
 export function MainForm() {
+  const [taskName, setTaskName] = useState("");
+
+  function handleCreateNewTask(event: React.FormEvent) {
+    event.preventDefault();
+    console.log("Deu certo");
+
+    console.log(taskName);
+  }
+
   return (
-    <form className="form" action="">
+    <form onSubmit={handleCreateNewTask} className="form">
       <div className="formRow">
         <DefaultInput
           labelText="task"
           id="meuInput"
           type="text"
           placeholder="Digite algo"
+          value={taskName}
+          onChange={(event) => setTaskName(event.target.value)}
         />
       </div>
 
