bee4f3c4f035ca2b39c2cbbbf76a4128fb7e5a7d
Heitor Macedo <heitorvmacedo@gmail.com>
2026-02-06 12:16:33 -0300

    59. Como iterar elementos JSX e usar a prop key no React (do jeito certo)

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,19 +1,29 @@
 import styles from "./styles.module.css";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function Cycles() {
+  const { state } = useTaskContext();
+
+  const cycleSteps = Array.from({ length: state.currentCycle });
+  const cycleDescriptionMap = {
+    workTime: "trabalho",
+    shortBreakTime: "pausa curta",
+    longBreakTime: "pausa longa",
+  };
+
   return (
     <div className={styles.cycles}>
       <span>Ciclos: </span>
 
       <div className={styles.cycleDots}>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.shortBreakTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.workTime}`}></span>
-        <span className={`${styles.cycleDot} ${styles.longBreakTime}`}></span>
+        {cycleSteps.map((_, index) => (
+          <span
+            key={index}
+            className={`${styles.cycleDot} ${styles[state.tasks[index].type]}`}
+            aria-label={`Indicador de ciclo de ${cycleDescriptionMap[state.tasks[index].type]}`}
+            title={`Ciclo de ${cycleDescriptionMap[state.tasks[index].type]}`}
+          ></span>
+        ))}
       </div>
     </div>
   );
